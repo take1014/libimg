2d50dc774a80ac8c2b6e997e240f8803032f2fd9
take1014 <y.takehara1014@gmail.com>
2014-08-22 09:58:00 +0900

    Add FFT

--- src/libimg.c
+++ src/libimg.c
@@ -15,13 +15,13 @@ img_init(img_struct *img){
    img->XDPI = 0;
    img->YDPI = 0;
    img->Pallet = NULL;
-   img->r = NULL;
+   img->r = NULL;/Users/takeharayuk/Desktop/libimg/src/libimg.h
    img->g = NULL;
    img->b = NULL;
 }
 
 /* ********************************************
-* 関数名　：img_malloc_array
+* 関数名　：img_uchar_malloc_array
 * 引数1 　：イメージ高さ
 * 引数2 　：イメージ横幅
 * 戻り値　：イメージ画素格納用の構造体
@@ -29,7 +29,7 @@ img_init(img_struct *img){
 * 　　　　　を行う。
 * *********************************************/
 static unsigned char**
-img_malloc_array(int Height, int Width){
+img_uchar_malloc_array(int Height, int Width){
    unsigned char **Brightness = (unsigned char**)malloc(sizeof(unsigned char*)*Height);
    if (Brightness == NULL){
       return NULL;
@@ -41,6 +41,36 @@ img_malloc_array(int Height, int Width){
       }
    }
 
+   for (int y = 0; y < Height; y++){
+      for (int x = 0; x < Width; x++){
+         /* 確保したメモリの初期化 */
+         Brightness[y][x] = 0x00;
+      }
+   }
+   return Brightness;
+}
+
+/* ********************************************
+ * 関数名　：img_double_malloc_array
+ * 引数1 　：イメージ高さ
+ * 引数2 　：イメージ横幅
+ * 戻り値　：イメージ画素格納用の構造体
+ * 処理概要：イメージの画素を格納する構造体の作成
+ * 　　　　　を行う。
+ * *********************************************/
+static double**
+img_double_malloc_array(int Height, int Width){
+   double **Brightness = (double**)malloc(sizeof(double*)*Height);
+   if (Brightness == NULL){
+      return NULL;
+   }
+   for (int y = 0; y < Height; y++){
+      Brightness[y] = (double*)malloc(sizeof(double)* Width);
+      if (Brightness[y] == NULL){
+         return NULL;
+      }
+   }
+   
    for (int y = 0; y < Height; y++){
       for (int x = 0; x < Width; x++){
          /* 確保したメモリの初期化 */
@@ -69,6 +99,265 @@ img_free(img_struct *img){
 }
 
 /* ********************************************
+ * 関数名　：img_calc_power_of_two
+ * 引数 　 ：イメージ構造体
+ * 戻り値　：乗数
+ * 処理概要：引数が２の何乗であるかを確認して
+ *           乗数を返却する。
+ * *********************************************/
+static int
+img_calc_power_of_two(int num){
+   /* 2の乗数 */
+   int power = 0;
+   while(num != 1){
+      /* 乗数カウント */
+      power++;
+      /* 右方向に1ビットシフト（2で割る） */
+      num = num >> 1;
+   }
+   /* 乗数を返却 */
+   return power;
+}
+
+/* ********************************************
+ * 関数名　：img_make_initial_data
+ * 引数1 　：元データ（実数部または虚数部）の先頭アドレス
+ * 引数3 　：データサイズ（縦幅または横幅）
+ * 引数4 　：乗数（data_sizeは2のpower乗）
+ * 戻り値　：なし
+ * 処理概要：FFTの初期データを作成する
+ * *********************************************/
+static void
+img_make_initial_data(double *data,int data_size,int power){
+   int ptr = 0;      /* 元データの要素番号を決めるための変数 */
+   int offset = 0;   /* 元データの要素番号を決めるためのオフセット */
+   int new_ptr = 0;  /* 計算後のデータを代入する配列の要素番号 */
+   int dft = 0;      /* DFT点数（iのループで半分ずつになる） */
+   double buf[data_size];     /* 計算結果格納用配列 */
+   
+   /* DFT点数にデータ総数を格納 */
+   dft = data_size;
+   /* 乗数の数だけループ */
+   for(int i = 1;i < power; i++){
+      /* 要素番号の初期化 */
+      new_ptr = 0;
+      /* オフセットの初期化 */
+      offset = 0;
+      /* データが存在する限りループ */
+      while(new_ptr < data_size){
+         /* 要素番号の初期化 */
+         ptr = 0;
+         /* 要素番号がDFT点数より小さくなるまでループ */
+         while(ptr < dft){
+            buf[new_ptr] = *(data + offset + ptr);
+            new_ptr++;
+            ptr = ptr + 2;
+            if(ptr == dft){
+               ptr = 1;
+            }
+            
+         }
+         /* オフセットのカウント */
+         offset = offset + dft;
+      }
+      
+      /* 計算結果を元のデータ配列にコピーする */
+      for(int j = 0;j < data_size; j++){
+         *(data + j) = buf[j];
+      }
+      dft = dft / 2;
+      if(dft % 2 != 0){
+         break;
+      }
+   }
+}
+
+
+/* ********************************************
+ * 関数名　：img_fft1
+ * 引数1 　：元データ（実数部または虚数部）の先頭アドレス
+ * 引数2 　：データサイズ（縦幅または横幅）
+ * 引数3 　：フーリエ変換ブラグ（１：FFT、-1：逆FFT）
+ * 引数4   ；データ種別（１：実数部、1以外：虚数部）
+ * 戻り値　：なし
+ * 処理概要：1次元フーリエ変換を行う
+ * *********************************************/
+static void
+img_fft1(double *data,int data_size,int flag,int type){
+   int power = 0;             /* べき乗 */
+   double unit_angle = 0;     /* 角度用 */
+   double step_angle = 0;     /* 角度用 */
+   int dft = 0;               /* DFT点数 */
+   int half = 0;              /* DFT点数の半数 */
+   int num_of_dft = 0;        /* DFT実行回数 */
+   int num_out = 0;           /* DFT出力信号線の番号 */
+   int num_in1 = 0;           /* DFT入力信号線の番号 */
+   int num_in2 = 0;           /* DFT入力信号線の番号 */
+   double buf = 0.0;       /* 実数部 */
+   double angle = 0.0;        /* 角度 */
+   
+   /* 計算結果を一時的に保存するための配列 */
+   double data_new[data_size];
+   
+   /* 逆FFTのとき、各データをnum_of_dataで割り複素共役を取る */
+   if(flag == -1){
+      for(int i = 0; i < data_size; i++){
+         *(data + i) = *(data + i) / data_size;
+      }
+   }
+   
+   /* num_of_dataが2の何乗であるかを調べる */
+   power = img_calc_power_of_two(data_size);
+   /* 初期データのための元データの順番の入れ替え */
+   img_make_initial_data(data,data_size,power);
+   /* 2点、4点、8点DFT・・・の順に実行する */
+   unit_angle = 2.0 * PI / data_size;
+   /* DFT点数の初期値→２倍していく */
+   dft = 2;
+   
+   for(int i = 0; i < power; i++){
+      /* DFT点のDFTを行う */
+      /* i=0 -> 2点、i=1 -> 4点、i=2 -> 8点・・・ */
+      num_of_dft = data_size / dft;     /* 実行回数 */
+      step_angle = unit_angle * num_of_dft;
+      half = dft / 2;
+      for(int j = 0; j < num_of_dft; j++){
+         angle = 0.0;
+         for(int k = 0; k < dft; k++){
+            /* num_in1,num_in2からnum_outを出力
+             （係数が1の方の信号をnum_in1としている）*/
+            num_out = j * dft + k;
+            if(k < half){
+               num_in1 = num_out;
+               num_in2 = num_in1 + half;
+            }else{
+               num_in2 = num_out;
+               num_in1 = num_out - half;
+            }
+            /* 実数部、虚数部に分けて計算する */
+            buf = *(data + num_in2);
+            if(type == 1){
+               /* 実数部の場合 */
+               data_new[num_out] = *(data + num_in1) + buf * cos(angle) + buf * sin(angle);
+            }
+            else{
+               /* 虚数部の場合 */
+               data_new[num_out] = *(data + num_in1) + buf * cos(angle) - buf * sin(angle);
+            }
+
+            /* 角度の更新 */
+            angle = angle + step_angle;
+         }
+      }
+      /* 計算後のデータを元の配列に戻す */
+      for(int j = 0; j < data_size; j++){
+         *(data + j) = data_new[j];
+      }
+      /* dftの数を２倍に更新 */
+      dft = dft * 2;
+   }
+   
+   /* 逆FFTの場合、各データの複素共役を取る */
+   if(flag == -1){
+      for(int j = 0; j < data_size; j++){
+         *(data + j) = -*(data + j);
+      }
+   }
+}
+
+/* ********************************************
+ * 関数名　：img_fft2
+ * 引数1 　：元データ（実数部）の先頭アドレス
+ * 引数2 　：元データ（虚数部）の先頭アドレス
+ * 引数3 　：フーリエ変換ブラグ（1：FFT、-1：逆FFT）
+ * 引数4 　：画像横幅
+ * 引数5 　：画像縦幅
+ * 戻り値　：なし
+ * 処理概要：2次元フーリエ変換を行う
+ * *********************************************/
+static void
+img_fft2(double **data,double **jdata,
+                        int flag,int x_size,int y_size){
+   double re[x_size];
+   double im[y_size];
+   
+   for(int y = 0; y < y_size; y++){
+      for(int x = 0; x < x_size; x++){
+         /* 実数部、虚数部のデータを取得 */
+         re[x] =  data[y][x];
+         im[x] = jdata[y][x];
+      }
+      /* 一次元フーリエ変換実施 */
+      img_fft1(re,x_size,flag,1);
+      img_fft1(im,y_size,flag,-1);
+      for(int x = 0; x < x_size; x++){
+         /* フーリエ変換したデータを格納 */
+         data[y][x] = re[x];
+         jdata[y][x] = im[x];
+      }
+   }
+   
+   for(int x = 0; x < x_size; x++){
+      for(int y = 0; y < y_size; y++){
+         /* 実数部、虚数部のデータを取得 */
+         re[x] =  data[y][x];
+         im[x] = jdata[y][x];
+      }
+      /* 一次元フーリエ変換実施 */
+      img_fft1(re,x_size,flag,1);
+      img_fft1(im,y_size,flag,-1);
+      for(int y = 0; y < y_size; y++){
+         /* フーリエ変換したデータを格納 */
+         data[y][x]  = re[y];
+         jdata[y][x] = im[y];
+      }
+      
+   }
+   
+}
+
+/* ********************************************
+ * 関数名　：img_make_fft_data
+ * 引数1 　：イメージ構造体
+ * 引数2 　：実数部データ
+ * 引数3 　：虚数部データ
+ * 戻り値　：なし
+ * 処理概要：フーリエ変換用データの作成を行う
+ * *********************************************/
+static void
+img_make_fft_data(img_struct img,double **data,double **jdata){
+   for(int y = 0; y < img.Height; y++){
+      for(int x= 0; x < img.Width; x++){
+         data[y][x] = (double)img.r[y][x];
+         jdata[y][x] = 0.0;
+      }
+   }
+}
+
+/* ********************************************
+ * 関数名　：img_fft_filter
+ * 引数1 　：実数部データ
+ * 引数2 　：虚数部データ
+ * 引数3 　：画像横幅
+ * 引数4 　：画像縦幅
+ * 戻り値　：なし
+ * 処理概要：周波数領域に対するフィルタリング
+ * *********************************************/
+static void
+img_fft_filter(double **data,double **jdata,int x_size,int y_size){
+   int max = 0;
+   max = 8;
+   for(int y = 0; y < y_size; y++){
+      for(int x = 0; x < x_size; x++){
+         if((max < y) && (y < (y_size - max)) || (max < x) && (x < (x_size -max))){
+            data[y][x] = 0.0;
+            jdata[y][x] = 0.0;
+         }
+      }
+   }
+}
+
+/* ********************************************
 * 関数名　：img_read
 * 引数1　 ：イメージファイルパス
 * 引数2　 ：イメージ構造体
@@ -76,7 +365,7 @@ img_free(img_struct *img){
 * 処理概要：イメージファイルの読込みを行う
 * *********************************************/
 int
-img_read(char *InputFileName, img_struct *img){
+img_read(char *inputfileName, img_struct *img){
    FILE *fp;                                           /* ファイルオープン用のポインタ     */
    int x = 0;                                          /* x方向の画像走査用                */
    int y = 0;                                          /* y方向の画像走査用                */
@@ -94,7 +383,7 @@ img_read(char *InputFileName, img_struct *img){
    img_init(img);
 
    /* 入力イメージのファイルオープン */
-   fp = fopen(InputFileName, "rb");
+   fp = fopen(inputfileName, "rb");
    /* ファイルオープンに失敗した場合 */
    if (fp == NULL){
       return ERR_IMG_READ_FILEOPEN;
@@ -145,9 +434,9 @@ img_read(char *InputFileName, img_struct *img){
       Data[i] = 0x00;
    }
 
-   img->r = img_malloc_array(img->Height, img->Width);
-   img->g = img_malloc_array(img->Height, img->Width);
-   img->b = img_malloc_array(img->Height, img->Width);
+   img->r =img_uchar_malloc_array(img->Height, img->Width);
+   img->g =img_uchar_malloc_array(img->Height, img->Width);
+   img->b =img_uchar_malloc_array(img->Height, img->Width);
    if (img->r == NULL || img->g == NULL || img->b == NULL){
       img_free(img);
       return ERR_IMG_READ_MEMORY;
@@ -316,7 +605,7 @@ img_read(char *InputFileName, img_struct *img){
 * 処理概要：イメージのファイル出力を行う
 * *********************************************/
 int
-img_write(char *OutputFileName, img_struct *img){
+img_write(char *outputfileName, img_struct *img){
    FILE *fp;                                       /* ファイルオープン用のポインタ */
    int x = 0;                                      /* x方向の画像走査用                */
    int y = 0;                                      /* y方向の画像走査用                */
@@ -336,7 +625,7 @@ img_write(char *OutputFileName, img_struct *img){
    int DataCount = 0;                          /* 書き込みデータ配列のカウント用 */
 
    /* 出力ファイルのオープン */
-   fp = fopen(OutputFileName, "wb");
+   fp = fopen(outputfileName, "wb");
    if (fp == NULL){
       return ERR_IMG_WRITE_FILEOPEN;
    }
@@ -711,7 +1000,7 @@ img_linertransform(img_struct *img){
 * 処理概要：組織的ディザ処理
 * **********************************************/
 int
-img_dither(img_struct *img, int DitherType){
+img_dither(img_struct *img, int dithertype){
    double dbWidth = 0.0;          /* 16段階画像の階調値の単位幅 */
    int NewGray = 0;          /* 新しい階調                 */
    int XBlock = 0;          /* 横ブロック数               */
@@ -725,7 +1014,7 @@ img_dither(img_struct *img, int DitherType){
       return ERR_IMG_DITHER_BITDEPTH;
    }
    /* ディザ行列型のチェック */
-   if (!(DitherType == 1 || DitherType == 2 || DitherType == 3)){
+   if (!(dithertype == 1 || dithertype == 2 || dithertype == 3)){
       return ERR_IMG_DITHER_DITHERTYPE;
    }
 
@@ -741,7 +1030,7 @@ img_dither(img_struct *img, int DitherType){
       }
    }
 
-   if (DitherType == 1){
+   if (dithertype == 1){
       /* ========== Bayer型の場合 ========== */
       for (int j = 0; j < 4; j++){
          for (int k = 0; k < 4; k++){
@@ -751,7 +1040,7 @@ img_dither(img_struct *img, int DitherType){
       /* =================================== */
 
    }
-   else if (DitherType == 2){
+   else if (dithertype == 2){
       /* ========== 網点型の場合 ========== */
       for (int j = 0; j < 4; j++){
          for (int k = 0; k < 4; k++){
@@ -825,20 +1114,20 @@ img_dither(img_struct *img, int DitherType){
 * 処理概要：手動による2値化処理
 * **********************************************/
 int
-img_binarize(img_struct *img, int Threshold){
+img_binarize(img_struct *img, int threshold){
    /* ８ビットグレースケール以外の場合 */
    if (img->BitDepth != 8){
       return ERR_IMG_BINARIZE_BITDEPTH;
    }
 
    /* 2値化閾値の判定 */
-   if (Threshold < -255 || Threshold > 255){
+   if (threshold < -255 || threshold > 255){
       return ERR_IMG_BINARIZE_THRESHOLD;
    }
 
    for (int y = 0; y < img->Height; y++){
       for (int x = 0; x < img->Width; x++){
-         if ((int)img->r[y][x] > Threshold){
+         if ((int)img->r[y][x] > threshold){
             img->r[y][x] = img->g[y][x] = img->b[y][x] = 0xFF;
          }
          else{
@@ -939,7 +1228,7 @@ img_difuse(img_struct *img){
 * 処理概要：誤差拡散処理を行う
 * **********************************************/
 int
-img_sobelfilter(img_struct *img, int FilterType){
+img_sobelfilter(img_struct *img, int filtertype){
    int x = 0;   /* ループ用変数x  */
    int y = 0;   /* ループ用変数y  */
    int i = 0;   /* ループ用変数i  */
@@ -948,7 +1237,7 @@ img_sobelfilter(img_struct *img, int FilterType){
    double PixelValue = 0.0;   /* 計算用         */
    double min = 0.0;   /* 計算値の最小値 */
    double max = 0.0;   /* 計算値の最大値 */
-   int        weight[3][3];   /* フィルタ用配列 */
+   int weight[3][3];   /* フィルタ用配列 */
 
    /* ８ビットグレースケール以外の場合 */
    if (img->BitDepth != 8){
@@ -966,11 +1255,14 @@ img_sobelfilter(img_struct *img, int FilterType){
    /* フィルタタイプの取得 */
    for (y = 0; y < 3; y++){
       for (x = 0; x < 3; x++){
-         if (FilterType == 0){
+         if (filtertype == 0){
             weight[y][x] = HorizontalSobel[y][x];
          }
-         else{
+         else if(filtertype == 1){
             weight[y][x] = VerticalSobel[y][x];
+         }
+         else{
+            return ERR_IMG_SOBELFILTER_FILTERTYPE;
          }
       }
    }
@@ -1018,5 +1310,132 @@ img_sobelfilter(img_struct *img, int FilterType){
    }
 
    return OK;
+}
+
+/* ********************************************
+* 関数名　：img_smooth
+* 引数1   ：イメージ構造体
+* 引数2 　：フィルタタイプ
+* 戻り値　：エラーコード
+* 処理概要：線形平滑化を行う
+* **********************************************/
+int img_smooth(img_struct *img,int filtertype){
+   double div_const = 0.0;       /* 最後に割る値 */
+   double new_value = 0.0;       /* 処理後の値   */
+   int x = 0;              /* ループ用変数x  */
+   int y = 0;              /* ループ用変数y  */
+   int i = 0;              /* ループ用変数i  */
+   int j = 0;              /* ループ用変数j  */
+   int weight[3][3];       /* フィルタ用配列  */
+
+   /* ８ビットグレースケール以外の場合 */
+   if (img->BitDepth != 8){
+      return ERR_IMG_SMOOTH_BITDEPTH;
+   }
+
+   if(filtertype == 0){
+      div_const = 9.0;
+   }
+   else if(filtertype == 1){
+      div_const = 10.0;
+   }
+   else{
+      return ERR_IMG_SMOOTH_FILTERTYPE;
+   }
+
+      /* フィルタ用配列の初期化 */
+   for (y = 0; y < 3; y++){
+      for (x = 0; x < 3; x++){
+         weight[y][x] = 0;
+      }
+   }
+
+   /* フィルタタイプの取得 */
+   for (y = 0; y < 3; y++){
+      for (x = 0; x < 3; x++){
+         if (filtertype == 0){
+            weight[y][x] = SmoothFilter1[y][x];
+         }
+         else{
+            weight[y][x] = SmoothFilter2[y][x];
+         }
+      }
+   }
+
+   /* フィルタリングの実施 */
+   for(y = 1; y < img->Height - 1; y++){
+      for(x = 1; x < img->Width - 1; x++){
+         new_value = 0.0;
+         for(i = -1; i < 2; i++){
+            for(j = -1; j < 2; j++){
+               new_value = new_value + (double)(weight[i + 1][j + 1] * (int)img->r[y + i][x + j]);
+            }
+            img->r[y][x] = img->g[y][x] = img->b[y][x] = (unsigned char )(new_value/div_const);
+         }
+      }
+   }
+
+   return OK;
+
+}
+
+
+/* ********************************************
+ * 関数名　：img_fft
+ * 引数1   ：イメージ構造体
+ * 戻り値　：エラーコード
+ * 処理概要：イメージに対してフーリエ変換を行う
+ * **********************************************/
+int
+img_fft(img_struct *img){
+   
+   /* ８ビットグレースケール以外の場合 */
+   if (img->BitDepth != 8){
+      return ERR_IMG_FFT_BITDEPTH;
+   }
+   
+   double  **data;         /* 実数部用 */
+   double **jdata;         /* 虚数部用 */
+   
+   /* 配列の作成 */
+   data =  img_double_malloc_array(img->Height, img->Width);
+   jdata = img_double_malloc_array(img->Height, img->Width);
+   
+   /* メモリの確保に失敗した場合はエラーとする */
+   if(data == NULL || jdata == NULL){
+      return ERR_IMG_FFT_MEMORY;
+   }
+   
+   /* フーリエ変換用データの作成 */
+   img_make_fft_data(*img,data,jdata);
+
+   /* 2次元フーリエ変換 */
+   img_fft2(data,jdata,1,img->Width,img->Height);
+   
+   /* 周波数領域でのフィルタリング */
+   img_fft_filter(data, jdata, img->Width, img->Height);
+   
+   /* 2次元逆フーリエ変換 */
+   img_fft2(data,jdata,-1,img->Width,img->Height);
+   
+   /* 変換後データの保存 */
+   for(int y = 0; y < img->Height; y++){
+      for(int x = 0; x < img->Width; x++){
+         if(data[y][x] < 0){
+            data[y][x] = 0;
+         }
+         if(data[y][x] > MAXBRIGHTNESS){
+            data[y][x] = MAXBRIGHTNESS;
+         }
+         img->r[y][x] = img->g[y][x] = img->b[y][x] = data[y][x];
+      }
+   }
+   
+   /* 確保したメモリの解放 */
+   free(data);
+   free(jdata);
+   
+   return OK;
+   
 }
 
